09283f93f6bfb70efcb84e61108e84590a0e9d29
Ahmad <ahmad.baalbaky@universite-paris-saclay.fr>
2023-11-02 17:14:22 +0100

    fix: wrong latest records shown for a player on the website

--- game_api/src/graphql/player.rs
+++ game_api/src/graphql/player.rs
@@ -149,7 +149,7 @@ impl Player {
 
         let query = format!(
             "SELECT * FROM global_records
-            WHERE player_id = ? AND game_id NOT LIKE '%_benchmark'
+            WHERE record_player_id = ? AND game_id NOT LIKE '%_benchmark'
             ORDER BY record_date {date_sort_by}
             LIMIT 100",
         );
