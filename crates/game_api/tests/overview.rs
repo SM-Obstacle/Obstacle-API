c20b0a084412360bdd8ee0d46e396ef84c0f85ff
Ahmad Baalbaky <ahmadjfbky@gmail.com>
2025-12-06 21:04:43 +0100

    fix competition ranking test based on wrong ordering
    Refs: #103.

--- crates/game_api/tests/overview.rs
+++ crates/game_api/tests/overview.rs
@@ -354,7 +354,7 @@ async fn competition_ranking() -> anyhow::Result<()> {
         let times = [1000, 3000, 3000, 5000, 7000];
         let datetime_offsets = [0, 2, 1, 0, 0];
 
-        let expected_player_ids = [1, 3, 2, 4, 5];
+        let expected_player_ids = [1, 2, 3, 4, 5];
         let expected_ranks = [1, 2, 2, 4, 5];
 
         let map_id = insert_sample_map(&db.sql_conn).await?;
